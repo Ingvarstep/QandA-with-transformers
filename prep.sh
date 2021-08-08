bee420266cbcf0fcae48f30d0e973392bc90b1eb
Ihor Stepanov <64355919+Ingvarstep@users.noreply.github.com>
2021-08-08 19:20:51 +0300

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,5 +1,6 @@
 #prepatation
 mkdir squad
+mkdir test
 wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json -O squad/train-v2.0.json
 wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json -O squad/dev-v2.0.json
 pip install transformers
